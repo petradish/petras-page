c80082168764ec09687993b1c925c7aa1f0c5dd5
Petra Laohakul <petradish@gmail.com>
2019-11-19 15:25:48 -0500

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from '../components/header'
+import Container from "../components/container"
+export default () => (
+    <Container>
+
+    <Header headerText="About Gatsby" />
+    <Header headerText="It's pretty cool" />
+    <p>Such wow. Very React.</p>
+
+  </Container>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,181 @@
-import React from "react"
+import ScrollAnim from 'rc-scroll-anim';
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+
+const ScrollParallax = ScrollAnim.Parallax;
+const ScrollElement = ScrollAnim.Element;
+
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      css: { backgroundColor: '#174270', height: 920 },
+      cssNoPosition: true,
+    };
+  }
+
+  setCss = (e) => {
+    const css = this.state.css;
+    if (this.state.cssNoPosition) {
+      css.position = 'fixed';
+      css.top = 0;
+    } else {
+      css.position = '';
+      css.top = '';
+    }
+    this.setState({
+      css,
+      cssNoPosition: !this.state.cssNoPosition,
+    });
+  };
+
+  render() {
+    return (
+    <div>
+      <div className="pack-page page0 ">
+        <div className="home-title">
+          <div className="page-title">
+           Hi, I'm Petra!
+          </div>
+          <ScrollParallax
+          animation={{ opacity: 1, scaleX: 1, scaleY: 1,  }}
+          style={{ opacity: 0, paddingTop: 2, fontSize: 20, transform: 'scale(0)', }}
+          className="text-description"
+        >
+           Developer | Teacher | Volunteer | Dancer
+        </ScrollParallax>
+        </div>
+      </div>
+
+      <div className="pack-page"
+        style={{
+          backgroundColor: '#fbe110',
+          position: 'relative',
+          overflow: 'hidden',
+          height: 800,
+        }}
+      >
+        <ScrollParallax
+          animation={{ rotate: 360 }}
+          style={{ transform: 'rotate(0)' }}
+          className="demo-content parallax-shape"
+        >
+          Developer
+        </ScrollParallax>
+        <ScrollParallax
+         animation={{ x: 0, opacity: 1 }}
+         style={{
+           transform: 'translateX(300px)',
+           opacity: 0,
+           backgroundColor: '#3FD8B8',
+           color: '#fff',
+         }}
+         className="demo-content parallax-shape"
+       >
+          Teacher
+        </ScrollParallax>
+        <ScrollParallax
+          animation={{ x: 0, opacity: 1 }}
+          style={{
+            transform: 'translateX(-300px)',
+            opacity: 0,
+            backgroundColor: '#F38EAD',
+            color: '#fff',
+          }}
+          className="demo-content parallax-shape"
+        >
+          Volunteer
+        </ScrollParallax>
+        <ScrollParallax
+          animation={{ rotateY: 360 }}
+          style={{ transform: 'perspective(200px) rotateY(0)', backgroundColor: '#0098CE' }}
+          className="demo-content parallax-shape"
+        >
+          Dancer
+        </ScrollParallax>
+</div>
+      <div className="pack-page"
+        style={{
+          backgroundColor: '#0097D0',
+          position: 'relative',
+          textAlign: 'center',
+          fontSize: 36,
+        }}
+      >
+        <div className="page2-title">Projects</div>
+        <ScrollParallax
+          animation={{ translateY: 120, opacity: 1, ease: 'linear', playScale: [0, 2] }}
+          style={{ transform: 'translateY(420px) scale(.8)', color: '#fff' }}
+        >
+          <img
+            src="https://os.alipayobjects.com/rmsportal/CrVCkwvtTQQvQHL.png"
+            style={{ width: 100 }}
+            alt="img"
+          />
+        </ScrollParallax>
+        <ScrollParallax
+          animation={{ translateY: 20, opacity: 1, ease: 'linear', playScale: [0, 1.5] }}
+          style={{ transform: 'translateY(380px) scale(.9)', color: '#fff' }}
+        >
+          <img
+            src="https://os.alipayobjects.com/rmsportal/sfmdyWNlweIvfUh.png"
+            style={{ width: 150 }}
+            alt="img"
+          />
+        </ScrollParallax>
+        <ScrollParallax
+          animation={{ translateY: -100, opacity: 1, ease: 'linear', playScale: [0, 1.2] }}
+          style={{ transform: 'translateY(320px)', color: '#fff' }}
+        >
+          <img
+            src="https://os.alipayobjects.com/rmsportal/sfmdyWNlweIvfUh.png"
+            style={{ width: 200 }}
+            alt="img"
+          />
+        </ScrollParallax>
+      </div>
+
+
+      <ScrollElement style={{ height: 2000 }} id="Scroll-Pack">
+        <ScrollParallax
+          className="pack-page"
+          location="Scroll-Pack"
+          animation={{
+            backgroundColor: '#0097D0',
+            playScale: [1, 2],
+            onStart: () => { this.setCss('start'); },
+            onCompleteBack: () => { this.setCss('back complete'); },
+           
+          }}
+          style={this.state.css}
+        >
+          <ScrollParallax
+            animation={{ translateX: '0%', playScale: [1, 2] }}
+            style={{
+              transform: 'translateX(-100%)',
+              backgroundColor: '#F38EAD',
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+            }}
+            location="Scroll-Pack"
+          />
+          <ScrollParallax
+            animation={[{ translateY: 0 }, { translateY: '450px' }]}
+            location="Scroll-Pack"
+            style={{
+              transform: 'translateY(300px)',
+              color: '#fff',
+              fontSize: 36,
+              textAlign: 'center',
+            }}
+          >
+            Thanks for visiting! Contact me at: petradish@gmail.com
+          </ScrollParallax>
+        </ScrollParallax>
+      </ScrollElement>
+    </div>);
+  }
+}
+
